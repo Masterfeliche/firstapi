a607d0d7b0e3163fca0340cd94fe8ffbb0cfdeab
Masterfeliche <165141911+Masterfeliche@users.noreply.github.com>
2025-07-19 04:29:05 -0700

    after adding some html

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def greet():
-    return {"message": "Hellow guys"
-                
-            }
-
+async def greet(request: Request):
+       return templates.TemplateResponse(
+        request=request, name="index.html", context={"id": id}
+    )
 @app.get("/about")
 async def about_me():
     return {
